a57b1b99baa753f2fc246dfde525f6a1d8fe5cc8
Schaback <philipp.schaback@gmx.de>
2018-06-26 19:22:17 +0200

    Added test commit for travis

--- src/main/java/edu/kit/iti/formal/pse2018/evote/view/Main.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/view/Main.java
@@ -3,6 +3,6 @@ package edu.kit.iti.formal.pse2018.evote.view;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test2");
     }
 }
