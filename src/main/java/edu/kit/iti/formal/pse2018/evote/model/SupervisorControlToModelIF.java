c73bb4d3f876159c2cc2ca1bf2be36341f3fd03b
Schaback <philipp.schaback@gmx.de>
2018-06-29 12:40:56 +0200

    Added missing seperator lines in SupervisorControlToModelIF and ConfigIssues

--- src/main/java/edu/kit/iti/formal/pse2018/evote/model/SupervisorControlToModelIF.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/model/SupervisorControlToModelIF.java
@@ -5,10 +5,16 @@ import edu.kit.iti.formal.pse2018.evote.utils.ElectionDataIF;
 public interface SupervisorControlToModelIF extends ControlToModelIF {
 
     public void importConfig(String path);
+
     public void exportConfig(String path);
+
     public void setVoters(String[] names);
+
     public boolean setElectionData(ElectionDataIF electionData);
+
     public boolean firstAuthentication(String username, String password);
+
     public String[] getVotes();
+
     public void startElection();
 }
