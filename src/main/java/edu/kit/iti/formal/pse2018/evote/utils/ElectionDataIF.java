760601686a52451ba7730f594e8f50dff5496456
DetaNexus <Philipp.schaback@gmx.de>
2018-06-29 13:32:29 +0200

    Added missing functions in ElectionDataIF

--- src/main/java/edu/kit/iti/formal/pse2018/evote/utils/ElectionDataIF.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/utils/ElectionDataIF.java
@@ -1,4 +1,25 @@
 package edu.kit.iti.formal.pse2018.evote.utils;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ElectionDataIF {
+
+    public String getName();
+
+    public String getDescription();
+
+    public VotingSystemType getVotingSystem();
+
+    public int getCandidates();
+
+    public List<String> getCandidateDescriptions();
+
+    public Date getStartDate();
+
+    public Date getEndDate();
+
+    public void getEndCondition();
+
+    public int getVoterCount();
 }
