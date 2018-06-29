3600ad6dacb783c0d9895014ee95bab29587f9ee
Schaback <philipp.schaback@gmx.de>
2018-06-29 12:40:56 +0200

    Added missing seperator lines in SupervisorControlToModelIF and ConfigIssues

--- src/main/java/edu/kit/iti/formal/pse2018/evote/utils/ConfigIssues.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/utils/ConfigIssues.java
@@ -3,7 +3,10 @@ package edu.kit.iti.formal.pse2018.evote.utils;
 public interface ConfigIssues {
 
     public String getNameIssue();
+
     public String getCandidateIssue();
+
     public String getVoterIssue();
+    
     public String getTimespanIssue();
 }
