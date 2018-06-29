5e2a8a7204cebc63016ae1576d78e53d442192de
DetaNexus <Philipp.schaback@gmx.de>
2018-06-29 15:06:31 +0200

    Fixed small whitespace error to appease the allmighty checkstyle

--- src/main/java/edu/kit/iti/formal/pse2018/evote/model/sdkconnection/transactions/QueryTransaction.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/model/sdkconnection/transactions/QueryTransaction.java
@@ -2,7 +2,7 @@ package edu.kit.iti.formal.pse2018.evote.model.sdkconnection.transactions;
 
 import org.hyperledger.fabric.sdk.HFClient;
 
-public abstract class QueryTransaction extends Transaction{
+public abstract class QueryTransaction extends Transaction {
 
     public QueryTransaction(HFClient client) {
         super(client);
