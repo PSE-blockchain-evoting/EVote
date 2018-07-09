1e43db2c9b0deec566658a08aaa0db088ffadf07
DetaNexus <Philipp.schaback@gmx.de>
2018-07-09 16:46:40 +0200

    Added json -> ElectionDataIF conversion

--- src/main/java/edu/kit/iti/formal/pse2018/evote/utils/CandidatePercentileCondition.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/utils/CandidatePercentileCondition.java
@@ -1,11 +1,26 @@
 package edu.kit.iti.formal.pse2018.evote.utils;
 
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonWriter;
+
 public class CandidatePercentileCondition extends ElectionEndCondition {
 
     private int percentage;
 
+    public CandidatePercentileCondition(int percentage) {
+        this.percentage = percentage;
+    }
+
     @Override
     public String asString() {
-        throw new UnsupportedOperationException(); //TODO: Implement this
+        StringWriter stringWriter = new StringWriter();
+        JsonWriter writer = Json.createWriter(stringWriter);
+        JsonObject endCondition = Json.createObjectBuilder().add("type", "CandidatePercentileCondition")
+                .add("percentage", this.percentage).build();
+        writer.writeObject(endCondition);
+        writer.close();
+        return stringWriter.toString();
     }
 }
--- src/main/java/edu/kit/iti/formal/pse2018/evote/utils/VoterPercentileCondition.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/utils/VoterPercentileCondition.java
@@ -1,11 +1,26 @@
 package edu.kit.iti.formal.pse2018.evote.utils;
 
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonWriter;
+
 public class VoterPercentileCondition extends ElectionEndCondition {
 
     private int percentage;
 
+    public VoterPercentileCondition(int percentage) {
+        this.percentage = percentage;
+    }
+
     @Override
     public String asString() {
-        throw new UnsupportedOperationException(); //TODO: Implement this
+        StringWriter stringWriter = new StringWriter();
+        JsonWriter writer = Json.createWriter(stringWriter);
+        JsonObject endCondition = Json.createObjectBuilder().add("type", "VoterPercentileCondition")
+                .add("percentage", this.percentage).build();
+        writer.writeObject(endCondition);
+        writer.close();
+        return stringWriter.toString();
     }
 }
