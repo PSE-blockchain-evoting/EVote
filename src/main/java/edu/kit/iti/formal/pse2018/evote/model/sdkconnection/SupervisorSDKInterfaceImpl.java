80cf75382e5acf0657ecbe7cd3dcc85769948238
DetaNexus <Philipp.schaback@gmx.de>
2018-08-22 14:34:13 +0200

    Removed redundant code

--- src/main/java/edu/kit/iti/formal/pse2018/evote/model/sdkconnection/SupervisorSDKInterfaceImpl.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/model/sdkconnection/SupervisorSDKInterfaceImpl.java
@@ -21,7 +21,6 @@ import edu.kit.iti.formal.pse2018.evote.exceptions.NetworkConfigException;
 import edu.kit.iti.formal.pse2018.evote.exceptions.NetworkException;
 import edu.kit.iti.formal.pse2018.evote.model.SDKEventListener;
 import edu.kit.iti.formal.pse2018.evote.model.SupervisorSDKInterface;
-import edu.kit.iti.formal.pse2018.evote.model.sdkconnection.transactions.AllVotesQuery;
 import edu.kit.iti.formal.pse2018.evote.model.sdkconnection.transactions.DestructionInvocation;
 import edu.kit.iti.formal.pse2018.evote.model.sdkconnection.transactions.InitializationInvocation;
 import edu.kit.iti.formal.pse2018.evote.utils.ConfigResourceBundle;
@@ -32,26 +31,19 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
 import org.hyperledger.fabric.sdk.Enrollment;
-import org.hyperledger.fabric.sdk.HFClient;
 import org.hyperledger.fabric.sdk.exception.CryptoException;
 import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
 import org.hyperledger.fabric.sdk.exception.ProposalException;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
-import org.hyperledger.fabric_ca.sdk.Attribute;
-import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
 import org.hyperledger.fabric_ca.sdk.HFCAClient;
 import org.hyperledger.fabric_ca.sdk.HFCAEnrollment;
 import org.hyperledger.fabric_ca.sdk.HFCAIdentity;
-import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
 import org.hyperledger.fabric_ca.sdk.exception.EnrollmentException;
 import org.hyperledger.fabric_ca.sdk.exception.IdentityException;
-import org.hyperledger.fabric_ca.sdk.exception.RegistrationException;
 
 
 /**
@@ -87,29 +79,13 @@ public class SupervisorSDKInterfaceImpl extends SDKInterfaceImpl implements Supe
         ResourceBundle bundle = ConfigResourceBundle.loadBundle("config");
         HFCAClient hfcaClient = createHFCAClient();
         Enrollment enrollment;
-        HFCAIdentity identity;
         try {
-            //ArrayList<Attribute> attributes = new ArrayList<>();
-            //attributes.add(new Attribute("test_attr", "test_val"));
-            //identity = hfcaClient.newHFCAIdentity(username);
-            //identity.setAttributes(attributes);
             enrollment = hfcaClient.enroll(username, password);
         } catch (EnrollmentException | org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException e) {
             throw new NetworkConfigException(e.getMessage());
         }
         AppUser appUser = new AppUser(username, bundle.getString("affiliation"), new HashSet<>(), username,
                 bundle.getString("mspID"), enrollment);
-        /*try {
-            identity.update(appUser);
-            for (HFCAIdentity hfcaIdentity : hfcaClient.getHFCAIdentities(appUser)) {
-                System.out.println(hfcaIdentity);
-                for (Attribute attribute : hfcaIdentity.getAttributes()) {
-                    System.out.println("\t" + attribute.getName() + ": " + attribute.getValue());
-                }
-            }
-        } catch (IdentityException | org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException e) {
-            throw new NetworkException(e.getMessage());
-        }*/
 
         FileOutputStream fos = new FileOutputStream(filePath);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -175,43 +151,7 @@ public class SupervisorSDKInterfaceImpl extends SDKInterfaceImpl implements Supe
             throw new NetworkConfigException(e.getMessage());
         }
     }
-/*
-    @Override
-    public void createUser(String name, String filePath) throws IOException,
-            edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException {
-        ResourceBundle bundle = ConfigResourceBundle.loadBundle("config");
-        RegistrationRequest request;
-        try {
-            HFCAIdentity identity = hfcaClient.newHFCAIdentity(name);
-            identity.create(this.appUser);
-        } catch (org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException | IdentityException e) {
-            throw new edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException(e.getMessage());
-        }
-        try {
-            request = new RegistrationRequest(name, bundle.getString("affiliation"));
-            //request.addAttribute();
-        } catch (Exception e) {
-            throw new edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException(e.getMessage());
-        }
-        String enrollmentSecret;
-        try {
-            enrollmentSecret = hfcaClient.register(request, this.appUser);
-        } catch (RegistrationException | org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException e) {
-            throw new edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException(e.getMessage());
-        }
-        HFCAEnrollment enrollment;
-        try {
-            enrollment = (HFCAEnrollment) hfcaClient.enroll(name, enrollmentSecret);
-        } catch (EnrollmentException | org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException e) {
-            throw new edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException(e.getMessage());
-        }
-        AppUser user = new AppUser(name, bundle.getString("affiliation"), new HashSet<>(), name,
-                bundle.getString("mspID"), enrollment);
-        FileOutputStream fos = new FileOutputStream(filePath);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(appUser);
-    }
-*/
+
     @Override
     public void createUser(String name, String filePath) throws IOException,
             edu.kit.iti.formal.pse2018.evote.exceptions.EnrollmentException {
