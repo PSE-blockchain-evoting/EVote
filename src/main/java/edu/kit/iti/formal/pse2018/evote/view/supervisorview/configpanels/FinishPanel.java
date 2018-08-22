1e2a9a2968343c547a07b07ee0cdd4ac21c9ce10
Achim <achim.kriso@student.kit.edu>
2018-08-22 12:29:08 +0200

    When all config issues solved, the error messages dissapear; closing #86

--- src/main/java/edu/kit/iti/formal/pse2018/evote/view/supervisorview/configpanels/FinishPanel.java
+++ src/main/java/edu/kit/iti/formal/pse2018/evote/view/supervisorview/configpanels/FinishPanel.java
@@ -208,6 +208,7 @@ public class FinishPanel extends ConfigPanel {
     @Override
     public void onActive() {
         updateData();
+        lblIssues.setText("");
         adapter.getConfirmedConfigListener().actionPerformed(null);
     }
 
