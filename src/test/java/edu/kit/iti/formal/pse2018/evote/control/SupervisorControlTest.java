3c603e50f4c7bb137dff3179c1fe1328867b6161
Artem Vasilev <artengo1111@inbox.ru>
2018-08-26 13:25:14 +0200

    Increased coverage for SupervisorTest

--- src/test/java/edu/kit/iti/formal/pse2018/evote/control/SupervisorControlTest.java
+++ src/test/java/edu/kit/iti/formal/pse2018/evote/control/SupervisorControlTest.java
@@ -22,26 +22,76 @@ import edu.kit.iti.formal.pse2018.evote.exceptions.NetworkException;
 import edu.kit.iti.formal.pse2018.evote.model.SupervisorControlToModelIF;
 import edu.kit.iti.formal.pse2018.evote.view.SupervisorControlToViewIF;
 import edu.kit.iti.formal.pse2018.evote.control.supervisorcontrol.SupervisorControl;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Locale;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class SupervisorControlTest {
+	private SupervisorControlToModelIF model;
+	private SupervisorControlToViewIF view;
+
+	@Before
+	public void tor() {
+		Locale.setDefault(new Locale("de", "DE"));
+		this.model = mock(SupervisorControlToModelIF.class);
+		this.view = mock(SupervisorControlToViewIF.class);
+	}
+
 	@Test
 	public void auth() throws NetworkException, AuthenticationException, InternalSDKException, NetworkConfigException {
-		Locale.setDefault(new Locale("de", "DE"));
         String login = "nick";
         String pass = "p@ss";
-		SupervisorControlToViewIF view = mock(SupervisorControlToViewIF.class);
-		SupervisorControlToModelIF model = mock(SupervisorControlToModelIF.class);
         when(view.getUsername()).thenReturn(login);
         when(view.getPassword()).thenReturn(pass);
 		SupervisorControl control = new SupervisorControl(view, model);
-		control.getFirstAuthenticationListener().actionPerformed(null);
+		when(model.isElectionInitialized()).thenReturn(false);
+		ActionListener authLi = control.getFirstAuthenticationListener();
+		assertTrue(authLi != null);
+		authLi.actionPerformed(null);
         verify(view, times(1)).showFrontpage();
-        verify(model, times(1)).firstAuthentication(login, pass);
+		verify(model, times(1)).firstAuthentication(login, pass);
+		when(model.isElectionInitialized()).thenReturn(true);
+		authLi.actionPerformed(null);
+		verify(view, times(1)).showResults();
+		when(model.firstAuthentication(anyString(), anyString())).thenThrow(NetworkException.class);
+		authLi.actionPerformed(null);
+		verify(view, times(1)).showError(anyString());
+
+		ActionListener l2 = control.getImportConfigListener();
+		assertTrue(l2 != null);
+
+		ActionListener l1 = control.getFinishElectionListener();
+		assertTrue(l1 != null);
+		ActionListener l3 = control.getExportConfigListener();
+		assertTrue(l3 != null);
+		ActionListener l5 = control.getConfirmedConfigListener();
+		assertTrue(l5 != null);
+		ActionListener l6 = control.getNewConfigListener();
+		assertTrue(l6 != null);
+		ActionListener l7 = control.getStartElectionListener();
+		assertTrue(l7 != null);
+		ActionListener l8 = control.getAuthenticationListener();
+		assertTrue(l8 != null);
+		ActionListener l9 = control.getLogoutListener();
+		assertTrue(l9 != null);
+	}
+
+	@Test
+    public void imp() throws IOException {
+		String path = "path/somepath";
+		when(view.getImportPath()).thenReturn(path);
+		SupervisorControl cont = new SupervisorControl(view, model);
+		ActionListener imp = cont.getImportConfigListener();
+		imp.actionPerformed(null);
+		verify(model, times(1)).importConfig(path);
+		doThrow(IOException.class)
+				.when(model)
+				.importConfig(anyString());
 	}
 }
